ce6fa2c1bf8835441297e2a67d84e4aa92451cb2
Hassan <muhammadhshafqat@gmail.com>
2026-01-15 14:10:04 +0500

    Layout system

--- dist/app.js
+++ dist/app.js
@@ -5,15 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
+const express_ejs_layouts_1 = __importDefault(require("express-ejs-layouts"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path_1.default.join(__dirname, '../public/views'));
+app.use(express_ejs_layouts_1.default);
+app.set('layout', 'base');
 app.get('/', (req, res) => {
     res.send('Hello Hassan World!');
 });
 app.get('/home', (req, res) => {
-    res.render('home');
+    res.render('home', { title: 'Home' });
 });
 app.listen(port, () => {
     return console.log(`Server: http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import path from 'path';
+import expressLayouts from 'express-ejs-layouts';
+
+
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../public/views'));
+app.use(expressLayouts); 
+app.set('layout', 'base');
 
 app.get('/', (req, res) => {
   res.send('Hello Hassan World!');
 });
 
 app.get('/home', (req, res) =>  {
-  res.render('home');
+  res.render('home', {title : 'Home'});
 });
 
 app.listen(port, () => {
