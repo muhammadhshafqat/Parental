2e32465a94fe6629237847e4b7d53e7574cca208
Hassan <muhammadhshafqat@gmail.com>
2026-01-15 22:56:17 +0500

    login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,5 @@
-import {Router} from 'express';
+import {Router, Request, Response} from 'express';
+import { supabase } from '../app';
 
 const router = Router();
 
@@ -8,6 +9,20 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req,res) =>{
   res.render('register', {title: 'Create Account'});
+})
+
+
+router.post('/login', async (req: Request, res:Response) => {
+    const {email, password} = req.body;
+    const {data, error} = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+    if (error) {
+        return res.render('login', {title: 'Login', error: error.message});
+    }
+
+    res.redirect('/dashboard');
 })
 
 export default router;
