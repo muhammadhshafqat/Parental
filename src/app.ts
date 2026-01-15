c14e39f015fb6761e1b10a97c9462383a07dd587
Hassan <muhammadhshafqat@gmail.com>
2026-01-15 14:14:15 +0500

    Views for auth

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,14 @@ app.get('/home', (req, res) =>  {
   res.render('home', {title : 'Home'});
 });
 
+app.get('/login', (req, res) => {
+  res.render('login', {title: 'Login'});
+})
+
+app.get('/register', (req,res) =>{
+  res.render('register', {title: 'Create Account'});
+})
+
 app.listen(port, () => {
   return console.log(`Server: http://localhost:${port}`);
 });
