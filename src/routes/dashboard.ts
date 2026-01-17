88032c115d3c91552e636ee99ca32c10dbc0e2b8
M-H-T-I <hassantariqirshad@gmail.com>
2026-01-17 16:10:52 +0500

    added title for dashboard

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -6,7 +6,7 @@ const dashRouter: Router = Router();
 // need to implement security middleware to prevent non-login users from accessing dashboard
 dashRouter.use('/', (reqs, res) => {
 
-    res.render('dashboard', {layout: 'dashboard-base'});
+    res.render('dashboard', {layout: 'dashboard-base', title: "dashboard"});
 
 });
 
