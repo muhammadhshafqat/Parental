74805c525a265cc8b0d1d05802d5e984794bb60a
M-H-T-I <hassantariqirshad@gmail.com>
2026-02-01 09:35:51 +0500

    fixed safety settings and created new chat function

--- src/utils/ai/ai.ts
+++ src/utils/ai/ai.ts
@@ -1,4 +1,4 @@
-import { GenerateContentParameters, GoogleGenAI, Models } from '@google/genai';
+import { GenerateContentParameters, GoogleGenAI, SafetySetting, HarmCategory, HarmBlockThreshold, Chat} from '@google/genai';
 import ENV from "../../ENV/ENV";
 
 
@@ -7,13 +7,31 @@ const aiClient = new GoogleGenAI({apiKey:ENV.geminiKey});
 const MODEL_ID = "gemini-2.5-flash";
 
 const system_instruction:string  = "You are an expert on child education. Use formal language.";
-const safetySettings = [
-    {category: "HARM_CATEGORY_SEXUAL", threshold: "BLOCK_LOW_AND_ABOVE"},
-    {category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_MEDIUM_AND_ABOVE"},
-    {category: "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold:"BLOCK_LOW_AND_ABOVE"},
-    {category: "HARM_CATEGORY_HARRASSMENT", threshold: "BLOCK_MEDIUM_AND_ABOVE"}
+const safetySettings:SafetySetting[] = [
+    {category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE},
+    {category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE},
+    {category: HarmCategory.HARM_CATEGORY_IMAGE_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE}
 
 ];
+
+// creates a new chat and returns it
+async function createNewChat(): Promise<Chat>{
+
+    const chat:Chat = await aiClient.chats.create(
+
+        {
+            model: MODEL_ID,
+            config: {
+                safetySettings: safetySettings,
+                systemInstruction: system_instruction
+            }
+        }
+
+    );
+
+    return chat;
+
+}
 
 
 async function sendPromptToApi(prompt: string){
