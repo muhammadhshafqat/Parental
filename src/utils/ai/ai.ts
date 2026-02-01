d2b789e2471ba64abf879b2e5ee32d089444d2e7
M-H-T-I <hassantariqirshad@gmail.com>
2026-02-01 09:18:07 +0500

    created system and safety settings

--- src/utils/ai/ai.ts
+++ src/utils/ai/ai.ts
@@ -1,15 +1,32 @@
 import { GenerateContentParameters, GoogleGenAI, Models } from '@google/genai';
 import ENV from "../../ENV/ENV";
-import { response } from 'express';
 
 
 // --------------------------------------- AI CLIENT --------------------------
 const aiClient = new GoogleGenAI({apiKey:ENV.geminiKey});
 const MODEL_ID = "gemini-2.5-flash";
+
+const system_instruction:string  = "You are an expert on child education. Use formal language.";
+const safetySettings = [
+    {category: "HARM_CATEGORY_SEXUAL", threshold: "BLOCK_LOW_AND_ABOVE"},
+    {category: "HARM_CATEGORY_HATE_SPEECH", threshold: "BLOCK_MEDIUM_AND_ABOVE"},
+    {category: "HARM_CATEGORY_SEXUALLY_EXPLICIT", threshold:"BLOCK_LOW_AND_ABOVE"},
+    {category: "HARM_CATEGORY_HARRASSMENT", threshold: "BLOCK_MEDIUM_AND_ABOVE"}
+
+];
+
+
 async function sendPromptToApi(prompt: string){
 
     const response = aiClient.models.generateContent({
         model: MODEL_ID,
-    } as GenerateContentParameters) 
+        config: {
+            systemInstruction: system_instruction,
+            safetySettings: safetySettings
+        },
+        contents: [{
+            role: "user",
+        }]
+    } as GenerateContentParameters);
 
 }
